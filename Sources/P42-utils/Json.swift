fb66872b460ebf1e292ffe3d734b414fd16eb606
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-10 15:50:24 +0200

    ddddd

--- Sources/P42-utils/Json.swift
+++ Sources/P42-utils/Json.swift
@@ -1,8 +1,41 @@
+
+
 //
-//  File.swift
-//  
+//  Json.swift
+//  shops24
 //
-//  Created by Diederick de Buck on 10/09/2024.
+//  Created by Diederick de Buck on 18/12/2022.
 //
 
 import Foundation
+
+
+public class Json {
+    
+    public static func toJsonString<T: Codable>(request: T) -> String? {
+        do {
+            let jsonData = try JSONEncoder().encode(request)
+            return String(data: jsonData, encoding: .utf8)!
+        } catch {
+            return nil
+        }
+    }
+    
+    public static func toJsonData<T: Codable>(request: T) -> Data? {
+        do {
+            return try JSONEncoder().encode(request)
+        } catch {
+            return nil
+        }
+    }
+    
+    public static func fromJsonData<T: Codable>(_ kind: T.Type, from response: Data) -> T? {
+        let decoder = JSONDecoder()
+        do {
+            return try decoder.decode(kind.self, from: response)
+        }
+        catch {
+            return nil
+        }
+    }
+}
