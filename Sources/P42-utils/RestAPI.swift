fb0fb3a0c7c92f13f13d1090a594d65b12f3f2a3
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-09-10 13:40:14 +0200

    dddd

--- Sources/P42-utils/RestAPI.swift
+++ Sources/P42-utils/RestAPI.swift
@@ -1,8 +1,51 @@
 //
-//  File.swift
-//  
+//  RestAPI.swift
+//  shops24
 //
-//  Created by Diederick de Buck on 10/09/2024.
+//  Created by Diederick de Buck on 18/04/2023.
 //
 
 import Foundation
+
+
+public enum ShopifyHeader: String {
+    case access_token = "X-Shopify-Access-Token"
+}
+
+public class RestAPI {
+    
+    /*
+     *  getRequest:
+     *      components -> URL with querystring parameters
+     *      secret -> keychain stored access token
+     *      meta -> helper object that keeps track of outstanding/completed IO's
+     *      handler -> invoked on completion, delegates IO completion to meta object
+     */
+    @MainActor
+    public static func getRequest(
+        components: URLComponents,
+        secret: String,
+        meta: Meta,
+        handler: @escaping (Data, Meta) -> Void
+    ) {
+        print("\(String(describing: type(of: self))).\(#function)")
+        guard let url = components.url else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.addValue(secret, forHTTPHeaderField: ShopifyHeader.access_token.rawValue)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            DispatchQueue.main.async {
+                handler(data, meta)
+            }
+        }
+        .resume()
+    }
+    
+}
