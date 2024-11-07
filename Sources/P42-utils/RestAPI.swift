4c88706e977d428b162add6e1a07d3e156b5f6b7
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-11-07 21:00:26 +0100

    added POST-request

--- Sources/P42-utils/RestAPI.swift
+++ Sources/P42-utils/RestAPI.swift
@@ -11,6 +11,21 @@ public enum ShopifyHeader: String {
     case access_token = "X-Shopify-Access-Token"
 }
 
+public enum HTTPHeader: String {
+    case contentType = "Content-Type"
+    case authorization = "Authorization"
+    case accept = "Accept"
+    case userAgent = "User-Agent"
+    case cacheControl = "Cache-Control"
+}
+
+public enum RequestType: String {
+    case get
+    case put
+    case post
+    case delete
+}
+
 public class RestAPI {
     
     /*
@@ -44,6 +59,36 @@ public class RestAPI {
             }
         }
         .resume()
+    }
+    
+    
+    @MainActor
+    public static func postRequest(
+        url: String,
+        secret: String,
+        meta: Meta,
+        jsonBody: [String: Any],
+        handler: @escaping (Data, Meta) -> Void
+    ) {
+        guard let url = URL(string: url) else {
+            return
+        }
+        var request = URLRequest(url: url)
+        request.httpMethod = RequestType.post.rawValue
+        request.addValue("Bearer \(secret)", forHTTPHeaderField: HTTPHeader.authorization.rawValue)
+        request.addValue("application/json", forHTTPHeaderField: HTTPHeader.contentType.rawValue)
+        request.httpBody = try? JSONSerialization.data(withJSONObject: jsonBody)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            DispatchQueue.main.async {
+                handler(data, meta)
+            }
+        }.resume()
     }
     
 }
