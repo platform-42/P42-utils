8da36367c047a084e78bd359d907ca505a29cf11
Diederick de Buck <diederick.de.buck@platform-42.com>
2024-11-25 18:20:40 +0100

    dddddd

--- Sources/P42-utils/Meta.swift
+++ Sources/P42-utils/Meta.swift
@@ -6,13 +6,11 @@
 //
 
 public enum MetaState {
-    case confirmed
     case nowait
 }
 
 public class Meta {
     public var resources: Int
-    public var confirmedCount: Int
     public var nowaitCount: Int
     
     public var isCompleted: Bool {
@@ -22,20 +20,14 @@ public class Meta {
     public init(_ resources: Int) {
         self.resources = resources
         self.nowaitCount = 0
-        self.confirmedCount = 0
     }
     
     public func rearm() {
         self.nowaitCount = self.resources
-        self.confirmedCount = self.resources
     }
     
     public func vsem(_ metaState: MetaState) {
         switch metaState {
-        case .confirmed:
-            if self.confirmedCount > 0 {
-                self.confirmedCount -= 1
-            }
         case .nowait:
             if self.nowaitCount > 0 {
                 self.nowaitCount -= 1
