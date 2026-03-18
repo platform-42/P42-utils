f976e113527e49b45d4c549919d3ba396f1f5e9e
platform-42 <142492114+platform-42@users.noreply.github.com>
2026-03-18 19:33:23 +0100

    ddddd

--- Sources/P42Utils/RestAPILoadable.swift
+++ Sources/P42Utils/RestAPILoadable.swift
@@ -19,23 +19,32 @@ public protocol RestAPILoadable {
 
 public extension RestAPILoadable {
 
+    /*
+     *  loadDataByGet had 2 escaping closures
+     *  because the onError closure is an optional one, it is implicit escaping
+     *  compiler prohibits to define it that way. why not emitting a warning is beyond me
+     */
     func loadDataByGet(
         urlComponents: URLComponents,
         accessToken: String,
-        handler: @escaping (Data) -> Void
+        handler: @escaping (Data) -> Void,
+        onError: ((Error) -> Void)? = nil
     ) async {
         guard let url = urlComponents.url else {
             fatalError("Invalid URL components")
         }
+
         do {
             let response = try await RestAPIAsync.getRequest(
                 url: url.absoluteString,
                 secret: accessToken,
                 kind: .shopify
             )
+
             handler(response)
         } catch {
             print("Unknown error: \(error.localizedDescription)")
+            onError?(error)
         }
     }
 }
